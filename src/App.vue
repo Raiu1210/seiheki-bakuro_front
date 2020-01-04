ed81be7ec419c05d7e6f9179faaec4bf25bac142
Raiu1210 <baio1484@gmail.com>
2020-01-04 11:54:35 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,88 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="hello">
+    <!-- SVG preview -->
+    <svg ref="svgCard" viewBox="0 0 200 100">
+      <rect x="0" y="0" width="200" height="100" fill="#fff" stroke="#ff69b4" stroke-width="15"></rect>
+      <text 
+      x="50%" 
+      y="50%" 
+      font-size="8px"
+      text-anchor="middle">{{msg}}</text>
+    </svg>
+
+    <!-- input for creating SVG -->
+    <input v-model="msg" type="text">
+    <button @click="create">create</button>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import firebase from 'firebase'
+
+// firebaseのconfig情報をペースト
+const firebaseConfig = {
+  apiKey: "AIzaSyCXqpWunMIA5H0tbSbRZOdjUVBOF0KIRGY",
+  authDomain: "seiheki-bakuro.firebaseapp.com",
+  databaseURL: "https://seiheki-bakuro.firebaseio.com",
+  projectId: "seiheki-bakuro",
+  storageBucket: "seiheki-bakuro.appspot.com",
+  messagingSenderId: "562186276613",
+  appId: "1:562186276613:web:a0cadba51b2e95a4a4716c",
+  measurementId: "G-7K8E3E7GBL"
+};
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+// svgをpngに変換
+const svg2imageData = (svgElement, successCallback, errorCallback) => {
+  const canvas = document.createElement('canvas')
+  canvas.width = 1200
+  canvas.height = 630
+  const ctx = canvas.getContext('2d')
+  const image = new Image()
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, 1200, 630)
+    successCallback(canvas.toDataURL())
+  }
+  image.onerror = (e) => {
+    errorCallback(e)
+  }
+  const svgData = new XMLSerializer().serializeToString(svgElement)
+  image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa(unescape(encodeURIComponent(svgData)))
 }
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Hello World',
+      uuid: '1', // 適当に採番する
+      description: 'Vue.jsとFirebaseでOGP生成アプリをつくってみます'
+    }
+  },
+  methods: {
+    async create() {
+      // refでsvgCardをsvgに設定しているのでthis.$refs.svgCardで要素を取れます
+      svg2imageData(this.$refs.svgCard, async (data) => {
+        const sRef = firebase.storage().ref()
+        const now = Date.now()
+        const fileRef = sRef.child(now + '.png')
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+        // Firebase Cloud Storageにアップロード
+        await fileRef.putString(data, 'data_url');
+        const url = await fileRef.getDownloadURL()
+        // console.log(url)
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+        // Firestoreに保存しておく
+        const card = db.collection('cards').doc(this.uuid)
+        await card.set({
+          url,
+          message: this.description
+        });
+      })
+
+      alert("画像生成したよ！")
+    }
+  }
 }
-</style>
+</script>
